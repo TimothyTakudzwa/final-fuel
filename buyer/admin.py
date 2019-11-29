95e38b6fdf8f5a0ca0f7c0609e6bfc79657e7d90
Nathan Nyabvure <nnyabvure@zbfh.co.zw>
2019-11-29 15:05:01 +0200

    resolving merge conflicts

--- buyer/admin.py
+++ buyer/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from buyer.models import User, Company, FuelRequest
-from supplier.models import ServiceStation, Depot
-=======
 from buyer.models import User, FuelRequest
 from company.models import Company
 from supplier.models import Subsidiaries
->>>>>>> eb9dae185ddf257ba2a5fdec3e02a1c963ef3c37
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('email', 'username','first_name', 'last_name', 'phone_number')
