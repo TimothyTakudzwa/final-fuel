58ce48b939a0c0a1c210acc1a48c871f1588052c
Ali-Taku <takudzwatarupiwa@gmail.com>
2019-12-06 09:42:32 +0200

    offer modal fixes

--- supplier/views.py
+++ supplier/views.py
@@ -78,10 +78,13 @@ def fuel_request(request):
         else:
             buyer_request.my_offer = 0
             buyer_request.offer_id = 0
-        if buyer_request.fuel_type == 'petrol':
-            buyer_request.price = fuel.petrol_price
+        if fuel:
+            if buyer_request.fuel_type == 'petrol':
+                buyer_request.price = fuel.petrol_price
+            else:
+                buyer_request.price = fuel.diesel_price
         else:
-            buyer_request.price = fuel.diesel_price
+            buyer_request.price = 0
     return render(request, 'supplier/accounts/fuel_request.html', {'requests':requests})
 
 
