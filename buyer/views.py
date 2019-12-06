97c53b18be27bcf2614983b630c2aa63aeaccba0
Timothy Ngorima <timothytakudzwa@gmail.com>
2019-12-06 18:13:15 +0200

    Finalizing on making a request

--- buyer/views.py
+++ buyer/views.py
@@ -212,23 +212,20 @@ def dashboard(request):
             print(form.errors)
 
             if form.is_valid():
-                print("----------dssddsfsdfds----------------------------------")
-
                 fuel_request = FuelRequest()
                 fuel_request.name = request.user       
                 fuel_request.amount = form.cleaned_data['amount']
                 fuel_request.fuel_type = form.cleaned_data['fuel_type']
-                fuel_request.usd = request.POST.get('usd') if request.POST.get('usd') == "True" else False
-                fuel_request.cash = request.POST.get('cash') if request.POST.get('cash') == "True" else False
-                fuel_request.ecocash = request.POST.get('ecocash') if request.POST.get('ecocash') == "True" else False
-                fuel_request.swipe = request.POST.get('swipe') if request.POST.get('swipe') == "True" else False
-               
+                fuel_request.usd = True if request.POST.get('usd') == "True" else False
+                fuel_request.cash = True if request.POST.get('cash') == "True" else False
+                fuel_request.ecocash = True if request.POST.get('ecocash') == "True" else False
+                fuel_request.swipe = True if request.POST.get('swipe') == "True" else False
                 fuel_request.delivery_method = form.cleaned_data['delivery_method']
-                fuel_request.delivery_address = request.POST.get('delivery_address')
-                fuel_request.storage_tanks = request.POST.get('storage_tanks') if request.POST.get('storage_tanks') == "True" else False
-                fuel_request.pump_required = request.POST.get('pump_required') if request.POST.get('pump_required') == "True" else False
-                fuel_request.dipping_stick_required =request.POST.get('dipping_stick_required') if request.POST.get('usd') == "True" else False
-                fuel_request.meter_required = request.POST.get('meter_required') if request.POST.get('usd') == "True" else False
+                fuel_request.delivery_address = True if request.POST.get('delivery_address') == "True" else False
+                fuel_request.storage_tanks = True if request.POST.get('storage_tanks') == "True" else False
+                fuel_request.pump_required = True if request.POST.get('pump_required') == "True" else False
+                fuel_request.dipping_stick_required = True if request.POST.get('usd') == "True" else False
+                fuel_request.meter_required = True if request.POST.get('usd') == "True" else False
                 fuel_request.wait = True
                 fuel_request.save()
                 print("----------Got to the save part ---------")
