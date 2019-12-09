0ff9e1d80d8626e6852a6e98605271f90f8e11f8
Kallou0 <nzenzamk@gmail.com>
2019-12-09 09:39:23 -0800

    taku's changes to list

--- whatsapp/helper_functions.py
+++ whatsapp/helper_functions.py
@@ -242,7 +242,7 @@ def view_requests_handler(user, message):
         for req in requests:
             fuel = str(req.fuel_type).capitalize()
             response_message = f'{response_message} *{i}.* {fuel} *{req.amount}* litres made on {req.date}\n'
-            user.fuel_updates_ids = user.fuel_updates_ids + str(req.id)
+            user.fuel_updates_ids = user.fuel_updates_ids + str(req.id) + " "
             user.save()
             i += 1        
         user.position = 1 
@@ -294,7 +294,7 @@ def view_offers_handler(user, message):
         for offer in offers:
             fuel = offer.request.fuel_type.capitalize()
             response_message = response_message + f'*{i}.* {fuel} *{offer.quantity}* litres at {offer.price} made on {offer.date}\n'
-            user.fuel_updates_ids = user.fuel_updates_ids + str(offer.id)
+            user.fuel_updates_ids = user.fuel_updates_ids + str(offer.id) + " "
             user.save()
             i += 1
         user.position = 1
