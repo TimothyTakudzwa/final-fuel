eb6c487258d83fd34f783b197eeb3a83758588b5
Kallou0 <nzenzamk@gmail.com>
2019-12-16 05:31:11 -0800

    final changes to supplier registration

--- supplier/views.py
+++ supplier/views.py
@@ -338,7 +338,7 @@ def verification(request, token, user_id):
                     if company_exists:
                         selected_company =Company.objects.filter(name=request.POST.get('company')).first()
                         user.company = selected_company
-                        user.is_active = True
+                        user.is_active = False
                         user.is_waiting = True
                         user.save()
                         TokenAuthentication.objects.filter(user=user).update(used=True)
