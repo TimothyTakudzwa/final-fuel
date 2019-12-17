3b3e8828d3d9b27abeac74d51e5704bbb123a267
Ali-Taku <takudzwatarupiwa@gmail.com>
2019-12-17 17:50:04 +0200

    collection address mandatory

--- supplier/views.py
+++ supplier/views.py
@@ -183,6 +183,7 @@ def offer(request, id):
     if request.method == "POST" and float(request.POST.get('price')) != 0 and float(request.POST.get('quantity')) != 0:
         fuel_request = FuelRequest.objects.get(id=id)
         fuel = FuelUpdate.objects.filter(relationship_id=request.user.subsidiary_id).first()
+        subsidiary = Subsidiaries.objects.filter(id=request.user.subsidiary_id).first()
         
         if fuel_request.fuel_type.lower() == 'petrol':
             available_fuel = fuel.petrol_quantity
@@ -204,7 +205,12 @@ def offer(request, id):
                 offer.ecocash = True if request.POST.get('ecocash') == "True" else False
                 offer.swipe = True if request.POST.get('swipe') == "True" else False
                 offer.delivery_method = request.POST.get('delivery_method')
-                offer.collection_address = request.POST.get('s_number') + " " + request.POST.get('s_name') + " " + request.POST.get('s_town')
+                delivery_method = request.POST.get('delivery_method')
+                collection_address = request.POST.get('s_number') + " " + request.POST.get('s_name') + " " + request.POST.get('s_town')
+                if not collection_address.strip() and delivery_method.lower() == 'self collection':
+                    offer.collection_address = subsidiary.address
+                else:
+                    offer.collection_address = collection_address
                 offer.pump_available = True if request.POST.get('pump_required') == "True" else False
                 offer.dipping_stick_available = True if request.POST.get('usd') == "True" else False
                 offer.meter_available = True if request.POST.get('usd') == "True" else False
@@ -250,7 +256,12 @@ def edit_offer(request, id):
                 offer.ecocash = True if request.POST.get('ecocash') == "True" else False
                 offer.swipe = True if request.POST.get('swipe') == "True" else False
                 offer.delivery_method = request.POST.get('delivery_method1')
-                offer.collection_address = request.POST.get('s_number') + " " + request.POST.get('s_name') + " " + request.POST.get('s_town')
+                delivery_method = request.POST.get('delivery_method')
+                collection_address = request.POST.get('s_number') + " " + request.POST.get('s_name') + " " + request.POST.get('s_town')
+                if not collection_address.strip() and delivery_method.lower() == 'self collection':
+                    offer.collection_address = subsidiary.address
+                else:
+                    offer.collection_address = collection_address
                 offer.pump_available = True if request.POST.get('pump_required') == "True" else False
                 offer.dipping_stick_available = True if request.POST.get('usd') == "True" else False
                 offer.meter_available = True if request.POST.get('usd') == "True" else False
