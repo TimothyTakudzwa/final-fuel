d14fe93216b351faa82b324875b8f4cd973cc490
Ali-Taku <takudzwatarupiwa@gmail.com>
2019-12-18 12:30:35 +0200

    notification objects

--- notification/models.py
+++ notification/models.py
@@ -5,7 +5,7 @@ from buyer.models import User
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='notification_name')
     message = models.CharField(max_length=5000)
-    offer = models.CharField(max_length=30, choices=(('REQUEST' , 'REQUEST'), ('OFFER' , 'OFFER')))
+    action = models.CharField(max_length=30, choices=(('REQUEST' , 'REQUEST'), ('OFFER' , 'OFFER')))
     reference_id = models.PositiveIntegerField(default=0)
     date = models.DateField(auto_now_add=True)
     time = models.TimeField(auto_now_add=True)
