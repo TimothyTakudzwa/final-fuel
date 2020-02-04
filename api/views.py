13e7f0eb5542ac5315499afc8bdcb331d4b85236
MarlvinzW <marlvinchihota@gmail.com>
2020-02-04 16:22:15 +0200

    api fix

--- api/views.py
+++ api/views.py
@@ -382,7 +382,7 @@ def view_comments(request):
         if all_comments:
             for comment in all_comments:
                 data = {
-                    'name': comment.user, 'comment': comment.comment, 'date': comment.date, 'time': comment.time
+                    'name': comment.user.username, 'comment': comment.comment, 'date': comment.date, 'time': comment.time
                 }
                 comments_data.append(data)
             return JsonResponse(list(comments_data), status=200, safe=False)
