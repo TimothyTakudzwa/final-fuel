484d87ee5b300259180767a4dc98b4c393ee6bc6
Kallou0 <nzenzamk@gmail.com>
2020-02-28 02:27:23 -0800

    transactions buyer

--- supplier/views.py
+++ supplier/views.py
@@ -708,7 +708,7 @@ def complete_transaction(request, id):
                 transaction_sord_update(request, user, transaction_quantity, 'SALE', 'Petrol', payment_type,
                                         transaction)
 
-                messages.success(request, "Proof of Payment Approved!, Please create a delivery schedule for the buyer.")
+                messages.success(request, "Proof of Payment Approved!, Please create a delivery schedule for the buyer or upload a release note.")
                 return redirect('transaction')
             else:
                 messages.warning(request, "There is not enough petrol in stock to complete the transaction.")
@@ -1044,8 +1044,16 @@ def upload_release_note(request, id):
     transaction = Transaction.objects.filter(id=id).first()
     if request.method == 'POST':
         transaction.release_note = request.FILES.get('release_note')
+
+        transaction.proof_of_payment = None
+        transaction.pending_proof_of_payment = False
         transaction.save()
-        messages.success(request, 'Release note uploaded successfully')
+        payment_history = AccountHistory.objects.filter(transaction=transaction, value=0.00).first()
+        payment_history.value += transaction.paid_reserve
+        payment_history.balance -= transaction.paid_reserve
+        payment_history.release_note = request.FILES.get('release_note')
+        payment_history.save()
+        messages.success(request, "Release Note Successfully Uploaded")
         return redirect('transaction')
 
 
