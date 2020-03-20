3d1f796ba4adc8f9ba981f3998d7967362fa90f3
Timothy Ngorima <timothytakudzwa@gmail.com>
2020-03-20 11:23:58 +0200

    Solving Attribute error or super admin landing page

--- users/views.py
+++ users/views.py
@@ -71,10 +71,11 @@ functions for allocating fuel to depots and stations
 def allocate(request):
     allocates = []
     fuel_object = DepotFuelUpdate.objects.first()
-    diesel_rtgs_price = fuel_object.rtgs_diesel_price
-    diesel_usd_price = fuel_object.usd_diesel_price
-    petrol_rtgs_price = fuel_object.rtgs_petrol_price
-    petrol_usd_price = fuel_object.usd_petrol_price
+    if fuel_object is not None:
+        diesel_rtgs_price = fuel_object.rtgs_diesel_price
+        diesel_usd_price = fuel_object.usd_diesel_price
+        petrol_rtgs_price = fuel_object.rtgs_petrol_price
+        petrol_usd_price = fuel_object.usd_petrol_price
     depots = NoicDepot.objects.all()
     company_capacity = CompanyFuelUpdate.objects.filter(company=request.user.company).first()
     subs_total_diesel_capacity = 0
