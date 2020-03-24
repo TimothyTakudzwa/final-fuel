a9761850d613816a240bdaa21e5e3efea5ba326a
Ali-Taku <takudzwatarupiwa@gmail.com>
2020-03-24 12:48:35 +0200

    password change fix

--- serviceStation/views.py
+++ serviceStation/views.py
@@ -48,7 +48,7 @@ def initial_password_change(request):
 
             messages.success(request, 'Password Successfully Changed')
             return redirect('serviceStation:home')
-    return render(request, 'supplier/initial_pass_change.html')
+    return render(request, 'serviceStation/initial_pass_change.html')
 
 
 @login_required()
