a0d4124e77c94ad0d1d4a37e7093a11cf355808a
tinomhishi <tinotendamhishi@protonmail.com>
2020-03-25 15:16:54 +0200

    commit for review

--- noicDepot/views.py
+++ noicDepot/views.py
@@ -68,6 +68,7 @@ def dashboard(request):
 def orders(request):
     depot = NoicDepot.objects.filter(id=request.user.subsidiary_id).first()
     orders = Order.objects.filter(noic_depot=depot).all()
+    # order_id = None
     return render(request, 'noicDepot/orders.html', {'orders': orders})
 
 def stock(request):
@@ -90,7 +91,7 @@ def payment_approval(request, id):
     order.payment_approved = True
     order.save()
     messages.success(request, 'payment approved successfully')
-    return redirect('noicDepot:orders')
+    return redirect(f'noicDepot/orders/?order_id={order.id}')
 
 def view_release_note(request, id):
     allocation = SordNationalAuditTrail.objects.filter(id=id).first()
