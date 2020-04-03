b903ad6cbb6c844f80820c16ae9e024a06af1ef1
MarlvinzW <marlvinchihota@gmail.com>
2020-04-03 17:10:23 +0200

    collections model,signal

--- noicDepot/models.py
+++ noicDepot/models.py
@@ -1,5 +1,13 @@
 from django.db import models
+from national.models import Order
 
 
 class Collections(models.Model):
-    pass
+    has_collected = models.BooleanField(default=False)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_for_collection')
+    date = models.DateField(auto_now_add=True)
+    time = models.TimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Collections'
+        ordering = ['date', 'time']
