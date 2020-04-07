d1852407926c80b081bcaa9cdfb4566e15ba9ce4
tinomhishi <tinotendamhishi@protonmail.com>
2020-04-07 18:35:42 +0200

    Fixing error on last commit

--- buyer/views.py
+++ buyer/views.py
@@ -1156,7 +1156,7 @@ def download_release_note(request, id):
 def download_pop(request, id):
     user_permission(request)
     transaction = Transaction.objects.filter(id=id).first()
-    document = AccountHistory.objects.filter(id=transaction).first()
+    document = AccountHistory.objects.filter(transaction=transaction).first()
     if document:
         filename = document.proof_of_payment.name.split('/')[-1]
         response = HttpResponse(document.proof_of_payment, content_type='text/plain')
