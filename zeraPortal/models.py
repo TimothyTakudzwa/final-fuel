65191627e2d951a5fcfd1d06683949e6f6a45c7b
Kallou0 <nzenzamk@gmail.com>
2020-04-08 22:23:39 -0700

    zera

--- zeraPortal/models.py
+++ zeraPortal/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class FuelPrices(models.Model):
+    rtgs_diesel_price = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+    rtgs_diesel_vat = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+    rtgs_diesel_duty = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+    rtgs_petrol_price = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+    rtgs_petrol_vat = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+    rtgs_petrol_duty = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+    usd_diesel_price = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+    usd_diesel_vat = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+    usd_diesel_duty = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+    usd_petrol_price = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+    usd_petrol_vat = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+    usd_petrol_duty = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+    
