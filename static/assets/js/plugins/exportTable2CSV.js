611909f550a718104ba2b27789e5018c4696e4c8
tinomhishi <tinotendamhishi@protonmail.com>
2020-04-23 14:25:08 +0200

    Testing bug fix

--- static/assets/js/plugins/exportTable2CSV.js
+++ static/assets/js/plugins/exportTable2CSV.js
@@ -31,11 +31,11 @@ function exportTableToCSV(filename) {
     for (var i = 0; i < rows.length; i++) {
         var row = [], cols = rows[i].querySelectorAll("td, th");
         
-        for (var j = 0; j < cols.length; j++)
-            if (row.push(cols[j].innerText != "View All Actions")){
-                row.push(cols[j].innerText);
-            }
-        csv.push(row.join(","));        
+        for (var j = 0; j < cols.length; j++) 
+            row.push(cols[j].innerText);
+            console.log("Debugging: ------->", cols[j].innerText);
+        
+        csv.push(row.join(";"));        
     }
 
     // Download CSV file
