f1ddaaffc32b7ca8c39928ed9437796ceb6585da
tinomhishi <tinotendamhishi@protonmail.com>
2020-05-06 20:09:56 +0200

    Fixing  UnboundLocalError

--- buyer/views.py
+++ buyer/views.py
@@ -802,6 +802,7 @@ Transaction Handlers
 def transactions(request):
 
     buyer = request.user
+    start_date, end_date = None
 
     all_transactions = Transaction.objects.filter(buyer=buyer).all()
     for transaction in all_transactions:
