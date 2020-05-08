496efca4515ab1f09f23732e5eeecd41fee33d1a
tinomhishi <tinotendamhishi@protonmail.com>
2020-05-08 12:02:49 +0200

    Fixed collections form appearance and added date filtering

--- noicDepot/views.py
+++ noicDepot/views.py
@@ -1004,10 +1004,12 @@ def statistics(request):
 @login_required()
 @user_role
 def collections(request):
+    filtered_collections = None
     context = {
         'collections': Collections.objects.exclude(date=today).order_by('-date', '-time'),
         'new_collections': Collections.objects.filter(date=today).order_by('-date', '-time'),
-        'form': CollectionsForm()
+        'form': CollectionsForm(),
+        'filtered_activities': filtered_collections
     }
     if request.method == 'POST':
         collection = Collections.objects.filter(id=request.POST.get('collection_id')).first()
@@ -1032,6 +1034,87 @@ def collections(request):
 
         messages.success(request, 'Collection saved successfully.')
         return redirect('noicDepot:collections')
+    
+        if request.POST.get('start_date') and request.POST.get('end_date') :
+            filtered = True;
+            start_date = request.POST.get('start_date')
+            end_date = request.POST.get('end_date')
+            if start_date:
+                start_date = datetime.strptime(start_date, '%Y-%m-%d')
+                start_date = start_date.date()
+            if end_date:
+                end_date = datetime.strptime(end_date, '%Y-%m-%d')
+                end_date = end_date.date()
+            
+            filtered_collections = Collections.objects.filter(date__range=[start_date, end_date])
+
+            context = {
+                'start_date': start_date,
+                'end_date': end_date,
+                'form': CollectionsForm(),
+                'filtered_collections': filtered_collections
+            }
+
+            return render(request, 'noicDepot/collections.html', context=context)
+
+
+        if request.POST.get('export_to_csv')=='csv':
+            start_date = request.POST.get('csv_start_date')
+            end_date = request.POST.get('csv_end_date')
+            if start_date:
+                start_date = datetime.strptime(start_date, '%b %d, %Y')
+                start_date = start_date.date()
+            if end_date:
+                end_date = datetime.strptime(end_date, '%b %d, %Y')
+                end_date = end_date.date()
+            if end_date and start_date:
+                filtered_activities = Activity.objects.filter(user=request.user).filter(date__range=[start_date, end_date])
+                      
+            fields = ['date','time', 'company__name', 'action', 'description', 'reference_id']
+            
+            if filtered_activities:
+                filtered_activities = filtered_activities.values('date','time', 'company__name', 'action', 'description', 'reference_id')
+                df = pd.DataFrame(filtered_activities, columns=fields)
+            else:
+                df_current = pd.DataFrame(current_activities.values('date','time', 'company__name', 'action', 'description', 'reference_id'), columns=fields)
+                df_previous = pd.DataFrame(activities.values('date','time', 'company__name', 'action', 'description', 'reference_id'), columns=fields)
+                df = df_current.append(df_previous)
+
+            filename = f'ZERA - {today}.csv'
+            df.to_csv(filename, index=None, header=True)
+
+            with open(filename, 'rb') as csv_name:
+                response = HttpResponse(csv_name.read())
+                response['Content-Disposition'] = f'attachment;filename={filename} - {today}.csv'
+                return response     
+
+        else:
+            start_date = request.POST.get('pdf_start_date')
+            end_date = request.POST.get('pdf_end_date')
+            if start_date:
+                start_date = datetime.strptime(start_date, '%b %d, %Y')
+                start_date = start_date.date()
+            if end_date:
+                end_date = datetime.strptime(end_date, '%b %d, %Y')
+                end_date = end_date.date()
+            if end_date and start_date:
+                filtered_activities = Activity.objects.filter(user=request.user).filter(date__range=[start_date, end_date])
+
+            html_string = render_to_string('zeraPortal/export/activities_export.html', {'filtered_activities': filtered_activities,
+            'start_date':start_date,'current_activities': current_activities, 'activities':activities, 'end_date':end_date,
+            'date':today})
+            html = HTML(string=html_string)
+            export_name = f"ZERA - {today}"
+            html.write_pdf(target=f'media/transactions/{export_name}.pdf')
+
+            download_file = f'media/transactions/{export_name}'
+
+            with open(f'{download_file}.pdf', 'rb') as pdf:
+                response = HttpResponse(pdf.read(), content_type="application/vnd.pdf")
+                response['Content-Disposition'] = f'attachment;filename={export_name} - Activities - {today}.pdf'
+                return response
+
+    
 
     return render(request, 'noicDepot/collections.html', context=context)
 
