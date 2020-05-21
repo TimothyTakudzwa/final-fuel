05ac13c1c5d4bcb801ff11f7066a1426335a6441
tinomhishi <tinotendamhishi@protonmail.com>
2020-05-21 13:55:27 +0200

    Trying fix to csv export name

--- zeraPortal/views.py
+++ zeraPortal/views.py
@@ -1649,10 +1649,14 @@ def ministry_statements(request):
                 fields = ['date','sord_no','fuel_type','currency','quantity','price', 'allocated_to__name',
                 'allocated_by__company__name']
                 df = pd.DataFrame(sord_audits, columns=fields)
+                df.columns = ['Date','Sord No','Fuel Type','Currency','Quantity','Price', 'Allocated To',
+                'Allocated By']
             else:
                 fields = ['date','sord_number','transaction__offer__request__fuel_type',
                 'transaction__offer__request__payment_method','transaction__offer__quantity', 'transaction__offer__price']
                 df = pd.DataFrame(sord_acc_history, columns=fields)
+                df.columns = ['Sate','Sord No','Fuel Type','Currency',
+                'Quantity', 'Price']
 
             filename = f'ZERA - {today}.csv'
             df.to_csv(filename, index=None, header=True)
