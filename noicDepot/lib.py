be88d569085c2570727bbf004a65497a09fd6a1d
tinomhishi <tinotendamhishi@protonmail.com>
2020-06-02 20:57:49 +0200

    Replacing get_weekly_sales with new method

--- noicDepot/lib.py
+++ noicDepot/lib.py
@@ -8,36 +8,15 @@ from buyer.models import User, FuelRequest
 from national.models import Order
 from company.models import CompanyFuelUpdate
 
+
 def get_week_days(date):
     '''
     Get This Weeks Dates
     '''
     return [date + timedelta(days=i) for i in range(0 - date.weekday(), 7 - date.weekday())]
 
-
+          
 def get_weekly_sales(this_week, depot):
-    '''
-    Get the company's weekly sales
-    '''
-    if this_week == True:
-        date = datetime.now().date()
-    else:
-        date = datetime.now().date() - timedelta(days=7)    
-    week_days = get_week_days(date)
-    weekly_data = {}
-    for day in week_days:
-        weeks_revenue = 0
-        day_trans = Order.objects.filter(date=day, payment_approved=True, noic_depot=depot)
-        if day_trans:
-            for tran in day_trans:
-                weeks_revenue += tran.amount_paid
-        else:
-            weeks_revenue = 0
-        weekly_data[day.strftime("%a")] = int(weeks_revenue)
-    return weekly_data              
-
-
-def new_get_weekly_sales(this_week, depot):
     '''
     Get the company's weekly sales
     '''
