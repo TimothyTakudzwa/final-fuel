150dabb5f2731d9eea202bafb00eacf8c4d23321
tinomhishi <tinotendamhishi@protonmail.com>
2020-06-02 20:49:11 +0200

    Add refactored fn

--- noicDepot/lib.py
+++ noicDepot/lib.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
 
+from django.db.models import Sum
+
 from supplier.models import Subsidiaries, Transaction, UserReview
 # from company.models import Company, FuelUpdate
 from buyer.models import User, FuelRequest
@@ -32,7 +34,32 @@ def get_weekly_sales(this_week, depot):
         else:
             weeks_revenue = 0
         weekly_data[day.strftime("%a")] = int(weeks_revenue)
-    return weekly_data               
+    return weekly_data              
+
+
+def new_get_weekly_sales(this_week, depot):
+    '''
+    Get the company's weekly sales
+    '''
+    #Determine the week you wish to consider, this week or last week
+    if this_week == True:
+        date = datetime.now().date()
+    else:
+        date = datetime.now().date() - timedelta(days=7)    
+    week_days = get_week_days(date)
+
+    weekly_data = {}
+    
+    for day in week_days:
+        weeks_revenue = Order.objects.filter(date=day, payment_approved=True, noic_depot=depot).aggregate(
+            total = Sum('amount_paid')
+        )['total'] 
+        if weeks_revenue:
+            weekly_data[day.strftime("%a")] = int(weeks_revenue)
+        else:
+            weekly_data[day.strftime("%a")] = 0.00    
+    
+    return weekly_data     
 
 
 def get_aggregate_monthly_sales(year, depot):
