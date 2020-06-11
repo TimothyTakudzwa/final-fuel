51499708ded055556d626103f8b51f19db8da223
tinomhishi <tinotendamhishi@protonmail.com>
2020-06-11 14:22:00 +0200

    Fixng faulty argument

--- users/views.py
+++ users/views.py
@@ -1181,7 +1181,7 @@ def statistics(request):
 
     # Get top Clients ranked by revenue USD and RTGS
     clients = get_top_clients(10, request.user.company, 'RTGS')
-    usd_clients = get_top_clients(10, request.user.company, 'RTGS')
+    usd_clients = get_top_clients(10, request.user.company, 'USD')
 
 
     return render(request, 'users/statistics.html', {'num_of_notifications': num_of_notifications, 'notifications': notifications,'clients': clients,
