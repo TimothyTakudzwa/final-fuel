e882e0e3bcdc6f387a4b271e166a5ba7c02f5645
tinomhishi <tinotendamhishi@protonmail.com>
2020-07-06 19:39:37 +0200

    ooooouuuuuuuuuu

--- whatsapp/views.py
+++ whatsapp/views.py
@@ -11,28 +11,24 @@ from .helper_functions import bot_action, send_message
 
 @csrf_exempt 
 def index(request):
+    response_message = ""    
+    data = json.loads(request.body)
+    message = data['messages'][0]['body']    
+    phone_number = data['messages'][0]['author'].split('@')[0]
+    if phone_number == '263718055061':
+        return HttpResponse('')
+    check = User.objects.filter(phone_number = phone_number).exists()
+        if check:
+    user = User.objects.filter(phone_number=phone_number).first()
+    print('--------------------', message, phone_number, user.stage, user.position)    
 
-    return redirect('http://159.65.66.59:8002/index')
+    if user.is_active:
+        response_message = bot_action(request, user, message) 
+    else:
+        response_message = "Your cannot use this, please create a buyer account or wait for approval if you have already registered"
+else:
+user = User.objects.create(phone_number=phone_number, stage='registration', position=1)
+response_message = bot_action(request, user, message)
 
-    
-    # response_message = ""    
-    # data = json.loads(request.body)
-    # message = data['messages'][0]['body']    
-    # phone_number = data['messages'][0]['author'].split('@')[0]
-    # if phone_number == '263718055061':
-    #     return HttpResponse('')
-    # check = User.objects.filter(phone_number = phone_number).exists()
-    # if check:
-    #     user = User.objects.filter(phone_number=phone_number).first()
-    #     print('--------------------', message, phone_number, user.stage, user.position)    
-
-    #     if user.is_active:
-    #         response_message = bot_action(request, user, message) 
-    #     else:
-    #         response_message = "Your cannot use this, please create a buyer account or wait for approval if you have already registered"
-    # else:
-    #     user = User.objects.create(phone_number=phone_number, stage='registration', position=1)
-    #     response_message = bot_action(request, user, message)
-
-    # send_message(phone_number, response_message)
-    # return HttpResponse(response_message)
+a send_message(phone_number, response_message)
+return HttpResponse(response_message)
